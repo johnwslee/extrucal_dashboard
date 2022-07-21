132f91d1cfa88f46126e12c6c14ecd6da8fa451b
johnwslee <john.ws.lee@gmail.com>
2022-07-21 13:29:56 +0900

    Adds throughput and cable part

--- src/app.py
+++ src/app.py
@@ -1,5 +1,250 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output
+from extrucal.extrusion import throughput_cal, throughput_table, throughput_plot
+from extrucal.cable_extrusion import cable_cal, cable_table, cable_plot
+from extrucal.tube_extrusion import tube_cal, tube_table, tube_plot
+from extrucal.rod_extrusion import rod_cal, rod_table, rod_plot
+from extrucal.sheet_extrusion import sheet_cal, sheet_table, sheet_plot
+import dash_bootstrap_components as dbc
 
-app = Dash()
-app.layout = html.Div('I am alive!!')
-app.run_server()
+# Setup app and layout/frontend
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+    [
+        html.H1('Extrucal Dashboard', style={'textAlign': 'center'}),
+        html.Br(),
+        dcc.Tabs(
+            [
+                dcc.Tab(
+                    label='압출량 계산',
+                    children=[
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('스크류 사이즈 [mm]'),
+                                        dcc.Input(id='screw_size', type='number', value=200),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('자재의 용융 밀도 [kg/m^3]'),
+                                        dcc.Input(id='melt_density', type='number', value=800),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('스크류 플라이트 개수'),
+                                        dcc.Input(id='n_flight', type='number', value=1),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최소 스크류 RPM'),
+                                        dcc.Input(id='min_rpm', type='number', value=5),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최대 스크류 RPM'),
+                                        dcc.Input(id='max_rpm', type='number', value=50),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('스크류 RPM 증가 단위'),
+                                        dcc.Input(id='delta_rpm', type='number', value=5),
+                                    ],
+                                    md=5
+                                ),
+                                dbc.Col(
+                                    [
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H3('예상 압출량 [kg/hr]'),
+                                        html.Iframe(
+                                            id='throughput_plot',
+                                            style={'border-width': '0', 'width': '100%', 'height': '75%'}
+                                            ),
+                                        html.H6('Note: 커서를 그래프 Data Point에 대면 실제 값의 확인이 가능합니다'),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H3('압출량 표'),
+                                        html.Iframe(
+                                            id='throughput_table',
+                                            style={'border-width': '0', 'width': '100%', 'height': '100%'}
+                                            ),
+                                    ],
+                                    md=7
+                                )
+                            ]
+                        )
+                    ]
+                ),
+                dcc.Tab(
+                    label='케이블 압출 목부량 계산',
+                    children=[
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    [
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('절연 외경 [mm]'),
+                                        dcc.Input(id='outer_d', type='number', value=10),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('절연 두께 [mm]'),
+                                        dcc.Input(id='thickness', type='number', value=2),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('자재 상온 밀도 [kg/mm^3]'),
+                                        dcc.Input(id='s_density', type='number', value=920),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('자재의 용융/상온 밀도 비율'),
+                                        dcc.Input(id='density_ratio', type='number', value=0.85),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최소 선속 [mpm]'),
+                                        dcc.Input(id='min_l_speed', type='number', value=1),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최대 선속 [mpm]'),
+                                        dcc.Input(id='max_l_speed', type='number', value=10),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('선속 증가 단위'),
+                                        dcc.Input(id='delta_l_speed', type='number', value=1),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최소 압출기 사이즈'),
+                                        dcc.Input(id='min_size', type='number', value=40),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('최대 압출기 사이즈'),
+                                        dcc.Input(id='max_size', type='number', value=100),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('압출기 사이즈 증가 단위'),
+                                        dcc.Input(id='delta_size', type='number', value=5),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H5('압출기 사이즈 대비 미터링 채널 깊이 비율'),
+                                        dcc.Input(id='depth_percent', type='number', value=0.05),
+                                    ],
+                                    md=5
+                                ),
+                                dbc.Col(
+                                    [
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H3('압출기 사이즈, 선속 별 필요 스크류 RPM'),
+                                        html.Iframe(
+                                            id='cable_plot',
+                                            style={'border-width': '0', 'width': '100%', 'height': '42%'}
+                                            ),
+                                        html.H6('Note: 커서를 그래프 Data Point에 대면 실제 값의 확인이 가능합니다'),
+                                        html.Br(),
+                                        html.Br(),
+                                        html.H3('스크류 RPM 표'),
+                                        html.Iframe(
+                                            id='cable_table',
+                                            style={'border-width': '0', 'width': '100%', 'height': '100%'}
+                                            ),
+                                    ],
+                                    md=7
+                                )
+                            ]
+                        )
+                    ]
+                ),
+                dcc.Tab(
+                    label='튜브 압출 목부량 계산'
+                ),
+                dcc.Tab(
+                    label='로드 압출 목부량 계산'
+                ),
+                dcc.Tab(
+                    label='시트 압출 목부량 계산'
+                )
+            ]
+        )
+    ]
+)
+
+# Setup callbacks/backend for Throughput Plot
+@app.callback(
+    Output('throughput_plot', 'srcDoc'),
+    Input('screw_size', 'value'),
+    Input('melt_density', 'value'),
+    Input('n_flight', 'value'),
+    Input('min_rpm', 'value'),
+    Input('max_rpm', 'value'),
+    Input('delta_rpm', 'value'),
+)
+def show_throughput_plot(screw_size, melt_density, n_flight, min_rpm, max_rpm, delta_rpm):
+    output = throughput_plot(screw_size, melt_density, n_flight=n_flight, min_rpm=min_rpm, max_rpm=max_rpm, delta_rpm=delta_rpm)
+    return output.to_html()
+
+# Setup callbacks/backend for Throughput Table
+@app.callback(
+    Output('throughput_table', 'srcDoc'),
+    Input('screw_size', 'value'),
+    Input('melt_density', 'value'),
+    Input('n_flight', 'value'),
+    Input('min_rpm', 'value'),
+    Input('max_rpm', 'value'),
+    Input('delta_rpm', 'value'),
+)
+def show_throughput_table(screw_size, melt_density, n_flight, min_rpm, max_rpm, delta_rpm):
+    output = throughput_table(screw_size, melt_density, n_flight=n_flight, min_rpm=min_rpm, max_rpm=max_rpm, delta_rpm=delta_rpm)
+    return output.to_html()
+
+# Setup callbacks/backend for Cable Plot
+@app.callback(
+    Output('cable_plot', 'srcDoc'),
+    Input('outer_d', 'value'),
+    Input('thickness', 'value'),
+    Input('s_density', 'value'),
+    Input('density_ratio', 'value'),
+    Input('min_l_speed', 'value'),
+    Input('max_l_speed', 'value'),
+    Input('delta_l_speed', 'value'),
+    Input('min_size', 'value'),
+    Input('max_size', 'value'),
+    Input('delta_size', 'value'),
+    Input('depth_percent', 'value'),
+)
+def show_cable_plot(
+    outer_d, thickness, s_density, density_ratio, min_l_speed, max_l_speed, delta_l_speed, 
+    min_size, max_size, delta_size, depth_percent
+    ):
+    output = cable_plot(
+        outer_d=outer_d, thickness=thickness, s_density=s_density, density_ratio=density_ratio, 
+        min_l_speed=min_l_speed, max_l_speed=max_l_speed, delta_l_speed=delta_l_speed, min_size=min_size,
+        max_size=max_size, delta_size=delta_size, depth_percent=depth_percent
+        )
+    return output.to_html()
+
+# Setup callbacks/backend for Cable Table
+@app.callback(
+    Output('cable_table', 'srcDoc'),
+    Input('outer_d', 'value'),
+    Input('thickness', 'value'),
+    Input('s_density', 'value'),
+    Input('density_ratio', 'value'),
+    Input('min_l_speed', 'value'),
+    Input('max_l_speed', 'value'),
+    Input('delta_l_speed', 'value'),
+    Input('min_size', 'value'),
+    Input('max_size', 'value'),
+    Input('delta_size', 'value'),
+    Input('depth_percent', 'value'),
+)
+def show_cable_table(
+    outer_d, thickness, s_density, density_ratio, min_l_speed, max_l_speed, delta_l_speed, 
+    min_size, max_size, delta_size, depth_percent
+    ):
+    output = cable_table(
+        outer_d=outer_d, thickness=thickness, s_density=s_density, density_ratio=density_ratio, 
+        min_l_speed=min_l_speed, max_l_speed=max_l_speed, delta_l_speed=delta_l_speed, min_size=min_size,
+        max_size=max_size, delta_size=delta_size, depth_percent=depth_percent
+        )
+    return output.to_html()
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
